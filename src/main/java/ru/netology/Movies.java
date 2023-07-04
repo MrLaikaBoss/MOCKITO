19d6d8b0c4afdecc52482b9744f2696d6be4fd0d
Daraganov Git-71 <denis.daraganov2013@yandex.ru>
2023-07-04 18:50:18 +0300

    first commit

---
+++ src/main/java/ru/netology/Movies.java
@@ -0,0 +1,42 @@
+package ru.netology;
+public class Movies {
+    private Movies[] movies;
+    private int limit;
+
+    public Movies() {
+        this.limit = 5;
+        this.movies = new Movies[0];
+    }
+
+    public Movies(int limit) {
+        this.limit = limit;
+        this.movies = new Movies[0];
+    }
+
+    public void add(Movies point) {
+        Movies[] tmp = new Movies[movies.length + 1];
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
+        }
+        tmp[tmp.length - 1] = point;
+        movies = tmp;
+    }
+
+    public Movies[] findAll() {
+        return movies;
+    }
+
+    public Movies[] findLast() {
+        int resultLength;
+        if (limit > movies.length) {
+            resultLength = movies.length;
+        } else {
+            resultLength = limit;
+        }
+        Movies[] result = new Movies[resultLength];
+        for (int i = 0; i < resultLength; i++) {
+            result[i] = movies[movies.length - 1 - i];
+        }
+        return result;
+    }
+}
---
+++ src/test/java/MoviesTest.java
@@ -0,0 +1,87 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.Movies;
+
+public class MoviesTest {
+
+    Movies manager = new Movies(3);
+    Movies film1 = new Movies();
+    Movies film2 = new Movies();
+    Movies film3 = new Movies();
+    Movies film4 = new Movies();
+    Movies film5 = new Movies();
+    Movies film6 = new Movies();
+    Movies film7 = new Movies();
+
+
+    @Test
+    public void FindAllTest() {
+        manager.add(film1);
+        manager.add(film2);
+        manager.add(film3);
+
+        Movies[] expected = {film1, film2, film3};
+        Movies[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void FindLastTest() {
+        manager.add(film1);
+        manager.add(film2);
+        manager.add(film3);
+
+        Movies[] expected = {film3, film2, film1};
+        Movies[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void FindLastIfEightInList() {
+        manager.add(film1);
+        manager.add(film2);
+        manager.add(film3);
+        manager.add(film4);
+        manager.add(film5);
+        manager.add(film6);
+        manager.add(film7);
+
+        Movies[] expected = {film7, film6, film5};
+        Movies[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void FindLastIfMoreThanLimit() {
+        Movies manager = new Movies();
+
+        manager.add(film1);
+        manager.add(film2);
+        manager.add(film3);
+        manager.add(film4);
+        manager.add(film5);
+        manager.add(film6);
+        manager.add(film7);
+
+
+        Movies[] expected = {film7, film6, film5, film4, film3};
+        Movies[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void IfLimitLargerArray() {
+        manager.add(film1);
+        manager.add(film2);
+
+        Movies[] expected = {film2, film1};
+        Movies[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
+
